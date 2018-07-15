dbe5458e6dfa7da4499deaef6ebb181b8bc2efc4
hwanginbeom <rydn2002@gmail.com>
2018-07-15 17:22:12 +0900

    peopleDTO

--- src/model/domain/Hospital_TotalDTO.java
+++ src/model/domain/Hospital_TotalDTO.java
@@ -1,8 +1,283 @@
 package model.domain;
 
 public class Hospital_TotalDTO {
-
+	private String loc;
+	private int gnr_Hospital;
+	private int gnr_Bed;
+	private int hospital;
+	private int h_Bed;
+	private int clinic_Hospital;
+	private int clinic_Bed;
+	private int aff_Hospital;
+	private int aff_Bed;
+	private int nur_Hospital;
+	private int nur_Bed;
+	private int snr_Hospital;
+	private int snr_Bed;
+	private int tub_Hospital;
+	private int tub_Bed;
+	private int mental_hospital;
+	private int mental_Bed;
+	private int dent_Hospital;
+	private int dent_Bed;
+	private int dent_Clinic_Hospital;
+	private int dent_Clinic_Bed;
+	private int herb_Bed;
+	private int herb_Clinic_Hospital;
+	private int herb_Clinic_Bed;
+	private int maternity_Hospital;
+	private int maternity_Bed;
+	public Hospital_TotalDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Hospital_TotalDTO(String loc, int gnr_Hospital, int gnr_Bed, int hospital, int h_Bed, int clinic_Hospital,
+			int clinic_Bed, int aff_Hospital, int aff_Bed, int nur_Hospital, int nur_Bed, int snr_Hospital, int snr_Bed,
+			int tub_Hospital, int tub_Bed, int mental_hospital, int mental_Bed, int dent_Hospital, int dent_Bed,
+			int dent_Clinic_Hospital, int dent_Clinic_Bed, int herb_Bed, int herb_Clinic_Hospital, int herb_Clinic_Bed,
+			int maternity_Hospital, int maternity_Bed) {
+		super();
+		this.loc = loc;
+		this.gnr_Hospital = gnr_Hospital;
+		this.gnr_Bed = gnr_Bed;
+		this.hospital = hospital;
+		this.h_Bed = h_Bed;
+		this.clinic_Hospital = clinic_Hospital;
+		this.clinic_Bed = clinic_Bed;
+		this.aff_Hospital = aff_Hospital;
+		this.aff_Bed = aff_Bed;
+		this.nur_Hospital = nur_Hospital;
+		this.nur_Bed = nur_Bed;
+		this.snr_Hospital = snr_Hospital;
+		this.snr_Bed = snr_Bed;
+		this.tub_Hospital = tub_Hospital;
+		this.tub_Bed = tub_Bed;
+		this.mental_hospital = mental_hospital;
+		this.mental_Bed = mental_Bed;
+		this.dent_Hospital = dent_Hospital;
+		this.dent_Bed = dent_Bed;
+		this.dent_Clinic_Hospital = dent_Clinic_Hospital;
+		this.dent_Clinic_Bed = dent_Clinic_Bed;
+		this.herb_Bed = herb_Bed;
+		this.herb_Clinic_Hospital = herb_Clinic_Hospital;
+		this.herb_Clinic_Bed = herb_Clinic_Bed;
+		this.maternity_Hospital = maternity_Hospital;
+		this.maternity_Bed = maternity_Bed;
+	}
+	public String getLoc() {
+		return loc;
+	}
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+	public int getGnr_Hospital() {
+		return gnr_Hospital;
+	}
+	public void setGnr_Hospital(int gnr_Hospital) {
+		this.gnr_Hospital = gnr_Hospital;
+	}
+	public int getGnr_Bed() {
+		return gnr_Bed;
+	}
+	public void setGnr_Bed(int gnr_Bed) {
+		this.gnr_Bed = gnr_Bed;
+	}
+	public int getHospital() {
+		return hospital;
+	}
+	public void setHospital(int hospital) {
+		this.hospital = hospital;
+	}
+	public int getH_Bed() {
+		return h_Bed;
+	}
+	public void setH_Bed(int h_Bed) {
+		this.h_Bed = h_Bed;
+	}
+	public int getClinic_Hospital() {
+		return clinic_Hospital;
+	}
+	public void setClinic_Hospital(int clinic_Hospital) {
+		this.clinic_Hospital = clinic_Hospital;
+	}
+	public int getClinic_Bed() {
+		return clinic_Bed;
+	}
+	public void setClinic_Bed(int clinic_Bed) {
+		this.clinic_Bed = clinic_Bed;
+	}
+	public int getAff_Hospital() {
+		return aff_Hospital;
+	}
+	public void setAff_Hospital(int aff_Hospital) {
+		this.aff_Hospital = aff_Hospital;
+	}
+	public int getAff_Bed() {
+		return aff_Bed;
+	}
+	public void setAff_Bed(int aff_Bed) {
+		this.aff_Bed = aff_Bed;
+	}
+	public int getNur_Hospital() {
+		return nur_Hospital;
+	}
+	public void setNur_Hospital(int nur_Hospital) {
+		this.nur_Hospital = nur_Hospital;
+	}
+	public int getNur_Bed() {
+		return nur_Bed;
+	}
+	public void setNur_Bed(int nur_Bed) {
+		this.nur_Bed = nur_Bed;
+	}
+	public int getSnr_Hospital() {
+		return snr_Hospital;
+	}
+	public void setSnr_Hospital(int snr_Hospital) {
+		this.snr_Hospital = snr_Hospital;
+	}
+	public int getSnr_Bed() {
+		return snr_Bed;
+	}
+	public void setSnr_Bed(int snr_Bed) {
+		this.snr_Bed = snr_Bed;
+	}
+	public int getTub_Hospital() {
+		return tub_Hospital;
+	}
+	public void setTub_Hospital(int tub_Hospital) {
+		this.tub_Hospital = tub_Hospital;
+	}
+	public int getTub_Bed() {
+		return tub_Bed;
+	}
+	public void setTub_Bed(int tub_Bed) {
+		this.tub_Bed = tub_Bed;
+	}
+	public int getMental_hospital() {
+		return mental_hospital;
+	}
+	public void setMental_hospital(int mental_hospital) {
+		this.mental_hospital = mental_hospital;
+	}
+	public int getMental_Bed() {
+		return mental_Bed;
+	}
+	public void setMental_Bed(int mental_Bed) {
+		this.mental_Bed = mental_Bed;
+	}
+	public int getDent_Hospital() {
+		return dent_Hospital;
+	}
+	public void setDent_Hospital(int dent_Hospital) {
+		this.dent_Hospital = dent_Hospital;
+	}
+	public int getDent_Bed() {
+		return dent_Bed;
+	}
+	public void setDent_Bed(int dent_Bed) {
+		this.dent_Bed = dent_Bed;
+	}
+	public int getDent_Clinic_Hospital() {
+		return dent_Clinic_Hospital;
+	}
+	public void setDent_Clinic_Hospital(int dent_Clinic_Hospital) {
+		this.dent_Clinic_Hospital = dent_Clinic_Hospital;
+	}
+	public int getDent_Clinic_Bed() {
+		return dent_Clinic_Bed;
+	}
+	public void setDent_Clinic_Bed(int dent_Clinic_Bed) {
+		this.dent_Clinic_Bed = dent_Clinic_Bed;
+	}
+	public int getHerb_Bed() {
+		return herb_Bed;
+	}
+	public void setHerb_Bed(int herb_Bed) {
+		this.herb_Bed = herb_Bed;
+	}
+	public int getHerb_Clinic_Hospital() {
+		return herb_Clinic_Hospital;
+	}
+	public void setHerb_Clinic_Hospital(int herb_Clinic_Hospital) {
+		this.herb_Clinic_Hospital = herb_Clinic_Hospital;
+	}
+	public int getHerb_Clinic_Bed() {
+		return herb_Clinic_Bed;
+	}
+	public void setHerb_Clinic_Bed(int herb_Clinic_Bed) {
+		this.herb_Clinic_Bed = herb_Clinic_Bed;
+	}
+	public int getMaternity_Hospital() {
+		return maternity_Hospital;
+	}
+	public void setMaternity_Hospital(int maternity_Hospital) {
+		this.maternity_Hospital = maternity_Hospital;
+	}
+	public int getMaternity_Bed() {
+		return maternity_Bed;
+	}
+	public void setMaternity_Bed(int maternity_Bed) {
+		this.maternity_Bed = maternity_Bed;
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Hospital_TotalDTO [loc=");
+		builder.append(loc);
+		builder.append(", gnr_Hospital=");
+		builder.append(gnr_Hospital);
+		builder.append(", gnr_Bed=");
+		builder.append(gnr_Bed);
+		builder.append(", hospital=");
+		builder.append(hospital);
+		builder.append(", h_Bed=");
+		builder.append(h_Bed);
+		builder.append(", clinic_Hospital=");
+		builder.append(clinic_Hospital);
+		builder.append(", clinic_Bed=");
+		builder.append(clinic_Bed);
+		builder.append(", aff_Hospital=");
+		builder.append(aff_Hospital);
+		builder.append(", aff_Bed=");
+		builder.append(aff_Bed);
+		builder.append(", nur_Hospital=");
+		builder.append(nur_Hospital);
+		builder.append(", nur_Bed=");
+		builder.append(nur_Bed);
+		builder.append(", snr_Hospital=");
+		builder.append(snr_Hospital);
+		builder.append(", snr_Bed=");
+		builder.append(snr_Bed);
+		builder.append(", tub_Hospital=");
+		builder.append(tub_Hospital);
+		builder.append(", tub_Bed=");
+		builder.append(tub_Bed);
+		builder.append(", mental_hospital=");
+		builder.append(mental_hospital);
+		builder.append(", mental_Bed=");
+		builder.append(mental_Bed);
+		builder.append(", dent_Hospital=");
+		builder.append(dent_Hospital);
+		builder.append(", dent_Bed=");
+		builder.append(dent_Bed);
+		builder.append(", dent_Clinic_Hospital=");
+		builder.append(dent_Clinic_Hospital);
+		builder.append(", dent_Clinic_Bed=");
+		builder.append(dent_Clinic_Bed);
+		builder.append(", herb_Bed=");
+		builder.append(herb_Bed);
+		builder.append(", herb_Clinic_Hospital=");
+		builder.append(herb_Clinic_Hospital);
+		builder.append(", herb_Clinic_Bed=");
+		builder.append(herb_Clinic_Bed);
+		builder.append(", maternity_Hospital=");
+		builder.append(maternity_Hospital);
+		builder.append(", maternity_Bed=");
+		builder.append(maternity_Bed);
+		builder.append("]");
+		return builder.toString();
+	}
 	
 	
-
 }
